310f33d8b246c78f5ed666218c511199cb99a112
prakhar.mehrotra <prakhar.mehrotra@mobgen.com>
2018-11-15 15:15:25 +0100

    added test for android chrome key input

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var inputBox = document.getElementById('inputBox');
 //var cypherString = 'qwertyuiopasdfghjklzxcvbnm';
-var cypherString = 'abcdefghijklmnopqrstuvwxyz';
+var originalOrder = 'abcdefghijklmnopqrstuvwxyz';
 var cypherString = 'wumseyqpdhxrtcfijkzoalvbng';
 
 
@@ -12,6 +12,10 @@ inputBox.addEventListener("keydown", function(e){
 		if(inputBox.value == 'open sesame'){
 			setTimeout(()=>{alert("The answer is not the result, but how you got there instead");}, 100);
 		}
+	}
+
+	else{
+		inputBox.value += e.code;
 	}
 });
 
@@ -26,5 +30,17 @@ var transformLetter = function(keyCode, cypher){
 	var letterIndex = keyCode - 65;
 	var calculatedLetter = cypher.charAt(letterIndex);
 	return calculatedLetter;
+};
+
+var transformInputBoxVal = function(inputBoxVal, cypher){
+	var newVal;
+	for (i=0; i<inputBoxVal.length; i++){
+		inputC = inputBoxVal.charAt(i);
+		for (j=0; j<originalOrder.length; j++){
+			if (inputC == originalOrder.charAt(j)){
+				console.log("Match");
+			}
+		}
+	}
 };
 
